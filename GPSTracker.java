93b5bdf365c21641d05df0f7419007bd86881c1d
Federico Sendra <federicosendra@gmail.com>
2015-11-24 19:58:33 -0300

    Initial Commit

---
+++ GPSTracker.java
@@ -0,0 +1,230 @@
+package com.fsendra.alnorte;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+
+/**
+	*Clase simple para inicializar, obtener coordenadas y parar location services, tanto de gps como location con network
+	*Se puede incluir la clase como un servicio e inicializarlo directamente
+	*
+	*Incluir permisos
+	*<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />    
+	*<uses-permission android:name="android.permission.INTERNET" />
+	*
+**/
+ 
+public class GPSTracker {
+ 
+    private final Context mContext;
+ 
+    // flag for GPS status
+    boolean isGPSEnabled = false;
+ 
+    // flag for network status
+    boolean isNetworkEnabled = false;
+ 
+    // flag for GPS status
+    boolean canGetLocation = false;
+ 
+    Location location; // location
+    double latitude; // latitude
+    double longitude; // longitude
+ 
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+ 
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 5; // 5 secs
+ 
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+ 
+    public GPSTracker(Context context) {
+        this.mContext = context;
+        getLocation();
+    }
+ 
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext
+                    .getSystemService(Context.LOCATION_SERVICE);
+             
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+ 
+             // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+					
+			//PARA CRITERIA CUSTOMIZABLE
+            /*final Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setSpeedRequired(true);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setCostAllowed(true);
+
+            final String bestProvider = locationManager.getBestProvider(criteria, true);
+
+            locationManager.requestLocationUpdates(bestProvider, 1, 0.0f, this);*/
+ 
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                // First get location from Network Provider
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+        return location;
+    }
+    
+    
+     
+    /**
+     * Stop using GPS listener
+     * Calling this function will stop using GPS in your app
+     * */
+    public void stopUsingGPS(){
+        if(locationManager != null){
+            locationManager.removeUpdates(locationListener);
+        }       
+    }
+     
+    /**
+     * Function to get latitude
+     * */
+    public double getLatitude(){
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+         
+        // return latitude
+        return latitude;
+    }
+     
+    /**
+     * Function to get longitude
+     * */
+    public double getLongitude(){
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+         
+        // return longitude
+        return longitude;
+    }
+     
+    /**
+     * Function to check GPS/wifi enabled
+     * @return boolean
+     * */
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+     
+    /**
+     * Function to show settings alert dialog
+     * On pressing Settings button will lauch Settings Options
+     * */
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+      
+        // Setting Dialog Title
+        alertDialog.setTitle("Ajustes GPS");
+  
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS esta deshabilitado. Ir a Ajustes y activarlo?");
+  
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Ajustes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+  
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+            }
+        });
+  
+        // Showing Alert Message
+        alertDialog.show();
+    }
+    
+	private final LocationListener locationListener = new LocationListener() {
+
+		public void onLocationChanged(Location pLocation) {
+		//Si se quiere usar con precision, en este ejemplo es de 20m
+			if (pLocation.getAccuracy() <= 20.0)
+	    	{
+	    		location = pLocation;
+	    	}
+	    	Log.i("Coordenadas", "Cambiaron coordenadas con precision: " + pLocation.getAccuracy() + " a valores "+
+	    			"longitud: " + pLocation.getLongitude() + " latitud: " + pLocation.getLatitude());
+
+		}
+
+		public void onProviderDisabled(String provider) {
+            canGetLocation = false;
+
+		}
+
+		public void onProviderEnabled(String provider) {
+            canGetLocation = true;
+
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+	};
+ 
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+Android Singleton GPSTracker
+=====================
+
+Simple class for initializing location services, start and stop obtaining coordinates. Works with GPS and/or network location.
+It includes an alert dialog to show when location is disable
+
+It can be used as a singleton class or as a service
+
+Include permissions
+  
+    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />    
+    <uses-permission android:name="android.permission.INTERNET" />
+ 
+
+For a custom criteria, define:
+
+			final Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setSpeedRequired(true);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setCostAllowed(true);
+
+            final String bestProvider = locationManager.getBestProvider(criteria, true);
+
+            locationManager.requestLocationUpdates(bestProvider, 1, 0.0f, this);
